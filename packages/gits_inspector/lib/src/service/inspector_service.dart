e56070c9e283b09b1e40f3ae0dbfb54b6a468fbe
Dena Meidina <dena@gits.id>
2022-06-25 15:09:27 +0700

    feat: add dartdoc services

--- packages/gits_inspector/lib/src/service/inspector_service.dart
+++ packages/gits_inspector/lib/src/service/inspector_service.dart
@@ -1,16 +1,30 @@
 import 'package:gits_inspector/gits_inspector.dart';
 import 'package:sqflite/sqflite.dart';
 
+/// Service helper for sqlite inspector.
 abstract class InspectorService {
+  /// The name of table sqlite
   static const String _table = 'inspector';
+
+  /// The name of column id in table inspector.
   static const String _id = 'id';
+
+  /// The name of column request json in table inspector.
   static const String _request = 'request';
+
+  /// The name of column response json in table inspector.
   static const String _response = 'response';
+
+  /// The name of column created_at in table inspector.
   static const String _createdAt = 'created_at';
+
+  /// The name of column updated_at in table inspector.
   static const String _updatedAt = 'updated_at';
 
+  /// The wrapper database sqlite.
   static Database? _db;
 
+  /// Return [Database] with open database sqlite.
   static Future<Database> _openDatabase() async {
     final databasesPath = await getDatabasesPath();
     String path = '$databasesPath/gits_inspector.db';
@@ -22,20 +36,23 @@ abstract class InspectorService {
     });
   }
 
+  /// Validate database stil opened, if close then open database.
   static Future<void> _validateDatabaseOpened() async {
     if (_db?.isOpen ?? false) return;
     _db = await _openDatabase();
   }
 
-  static Future<void> insert(Inspector overviewInspector) async {
+  /// Insert data to sqlite with given [inspector].
+  static Future<void> insert(Inspector inspector) async {
     await _validateDatabaseOpened();
     await _db?.insert(
       _table,
-      overviewInspector.toMap(),
+      inspector.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
+  /// Update data to sqlite with given [inspector].
   static Future<void> updateResponse(
       String uuid, ResponseInspector response) async {
     await _validateDatabaseOpened();
@@ -50,6 +67,7 @@ abstract class InspectorService {
     );
   }
 
+  /// Return list [Inspector] with given [limit] and [offset].
   static Future<List<Inspector>> getAll({int? limit, int? offset}) async {
     await _validateDatabaseOpened();
     final listMap = await _db?.query(
@@ -62,6 +80,7 @@ abstract class InspectorService {
     return listMap.map((e) => Inspector.fromMap(e)).toList();
   }
 
+  /// Return [Inspector] with given [id].
   static Future<Inspector?> get(String id) async {
     await _validateDatabaseOpened();
     final listMap =
@@ -70,16 +89,19 @@ abstract class InspectorService {
     return null;
   }
 
+  /// Delete data sqlite with given [id].
   static Future<int?> delete(String id) async {
     await _validateDatabaseOpened();
     return await _db?.delete(_table, where: '$_id = ?', whereArgs: [id]);
   }
 
+  /// Delete all data sqlite inspector.
   static Future<void> deleteAll() async {
     await _validateDatabaseOpened();
     await _db?.delete(_table);
   }
 
+  /// Close the database if databse stil open.
   Future<void> close() async {
     if (_db?.isOpen ?? false) {
       await _db?.close();
