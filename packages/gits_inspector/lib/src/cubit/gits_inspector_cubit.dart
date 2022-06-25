2d33bf415f498a856948665ba7c2b1496984ff83
Dena Meidina <dena@gits.id>
2022-06-25 15:08:40 +0700

    feat: add dartdoc cubit

--- packages/gits_inspector/lib/src/cubit/gits_inspector_cubit.dart
+++ packages/gits_inspector/lib/src/cubit/gits_inspector_cubit.dart
@@ -8,6 +8,7 @@ import 'package:gits_inspector/src/service/inspector_service.dart';
 
 part 'gits_inspector_state.dart';
 
+/// Controller for all logic [GitsInspectorPage]
 class GitsInspectorCubit extends GitsCubit<GitsInspectorStateCubit> {
   GitsInspectorCubit()
       : super(const GitsInspectorStateCubit(
@@ -15,6 +16,7 @@ class GitsInspectorCubit extends GitsCubit<GitsInspectorStateCubit> {
           isSearchMode: false,
         ));
 
+  /// The master data list of [Inspector]
   late List<Inspector> masterListInspector;
 
   @override
@@ -25,22 +27,26 @@ class GitsInspectorCubit extends GitsCubit<GitsInspectorStateCubit> {
   @override
   void initAfterFirstLayout(BuildContext context) {}
 
+  /// Emit list of [Inspector] get all data from local data
   void getAllInspector() async {
     final list = await InspectorService.getAll();
     masterListInspector = List.from(list);
     emit(state.copyWith(inspectors: list));
   }
 
+  /// Emit list of [Inspector] to empty and delete all data from local
   void deleteAll() async {
     await InspectorService.deleteAll();
     emit(state.copyWith(inspectors: []));
   }
 
+  /// Emit toggle to change search mode
   void onChangeToSearch() => emit(state.copyWith(
         isSearchMode: !state.isSearchMode,
         inspectors: state.isSearchMode ? masterListInspector : null,
       ));
 
+  /// Emit list of [Inspector] when on change from search with give [value]
   void onSearchChanged(String value) {
     if (value.isEmpty) {
       emit(state.copyWith(inspectors: masterListInspector));
@@ -53,6 +59,7 @@ class GitsInspectorCubit extends GitsCubit<GitsInspectorStateCubit> {
     emit(state.copyWith(inspectors: search));
   }
 
+  /// Navigate to [GitsInspectorDetailPage]
   void navigateToDetail(BuildContext context, Inspector inspector) =>
       GitsInspectorDetailPage.navigate(context, inspector);
 }
--- packages/gits_inspector/lib/src/cubit/gits_inspector_state.dart
+++ packages/gits_inspector/lib/src/cubit/gits_inspector_state.dart
@@ -1,14 +1,19 @@
 part of 'gits_inspector_cubit.dart';
 
+/// State Cubit for [GitsInspectorCubit]
 class GitsInspectorStateCubit extends Equatable {
   const GitsInspectorStateCubit({
     required this.inspectors,
     required this.isSearchMode,
   });
 
+  /// The list of [Inspector] will used to show list data inspector.
   final List<Inspector> inspectors;
+
+  /// Flag used to toggle search mode.
   final bool isSearchMode;
 
+  /// Return copy of [GitsInspectorStateCubit] with given [inspectors] and [isSearchMode].
   GitsInspectorStateCubit copyWith({
     List<Inspector>? inspectors,
     bool? isSearchMode,
